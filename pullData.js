7a2fd0e7e766891227798981ed71ff23045d751a
Ryan Spicer <rspicer@razegroup.com>
2015-09-01 13:16:11 -0600

    pullData.js basic functionality

--- pullData.js
+++ pullData.js
@@ -2,17 +2,63 @@ var http = require('http');
 
 var key = 'VaeqiShEf3nbUnawWkEw';
 
+var dbConfig = {
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: 'sqltest',
+    database: 'rrapp',
+    charset: 'utf8',
+    debug: true
+  }
+};
+
+var knex = require('knex')(dbConfig);
+var bookshelf = require('bookshelf')(knex);
+
+
+
+var csRacer = bookshelf.Model.extend({
+	tableName: 'csracers'
+})
+
+var csRacers = bookshelf.Collection.extend({
+	model: csRacer
+})
+
 var options = {
 	host: 'aisbaltimore.clubspeedtiming.com',
 	path: '/api/index.php/racers/1000002?key=' + key
 }
 
 http.get(options, function(res) {
-	var finalResponse;
+	var finalResponse = '';
 	res.on('data', function(chunk) {
 		finalResponse += chunk;
 		console.log(finalResponse);
+		var parsed = JSON.parse(finalResponse);
+		console.log(parsed);
+		console.log(parsed.racer.id);
+		csRacer.forge({
+			racerid: parsed.racer.id,
+			name: parsed.racer.name.nickname,
+			rpm: parsed.racer.rpm,
+			// created_at: parsed.racer.created_at,
+			visits: parsed.racer.visits,
+			races: parsed.racer.races		
+		}).save()
+		// // knex('csracers').insert({
+		// 	id: parsed.racer.id,
+		// 	name: parsed.racer.name.nickname,
+		// 	rpm: parsed.racer.rpm,
+		// 	// created_at: parsed.racer.created_at,
+		// 	visits: parsed.racer.visits,
+		// 	races: parsed.racer.races
+		// })
 	});
 }).on('error', function(e) {
 	console.log('Got error:' + e.message);
+}).on('end', function() {
+	console.log('Done!');
 })
--- schema.js
+++ schema.js
@@ -1,9 +1,17 @@
 var Schema = {
-	races: {
-		id: {type: 'increments', nullable: false, primary: true},
-		raceid: {type: 'integer', nullable: false, unsigned: true},
-		racer : {type: 'string', maxlength: 50, nullable: false},
-		track: {type: 'string', nullable: false}
+	// races: {
+	// 	id: {type: 'increments', nullable: false, primary: true},
+	// 	raceid: {type: 'integer', nullable: false, unsigned: true},
+	// 	racer : {type: 'string', maxlength: 50, nullable: false},
+	// 	track: {type: 'string', nullable: false}
+	// },
+	csracers: {
+		racerid: {type: 'integer', nullable: false, unsigned: true},
+		name: {type: 'string', maxlength: 50, nullable: false},
+		rpm: {type: 'integer', nullable: false, unsigned: true},
+		// created_at: {type: 'date', nullable: false},
+		visits: {type: 'integer', nullable: false, unsigned: true},
+		races: {type: 'integer', nullable: false, unsigned: true}
 	}
 };
 
